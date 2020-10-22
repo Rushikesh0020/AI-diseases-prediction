0d90e4d1d6a92d4cf6cb9050999b6032758df28a
desaidj <desaidj96@outlook.com>
2020-10-22 10:07:03 +0530

    changing filepath

--- app.py
+++ app.py
@@ -20,9 +20,9 @@ def convert_img(image_path, target_size=(128,128)):
     img_arr_img.append(image)
     return np.asarray(img_arr_img)
 
-reconstructed_model = keras.models.load_model("../AI PBL project/pretrained models/cnn_malaria.h5")
+reconstructed_model = keras.models.load_model("../AI-diseases-prediction-master/pretrained models/cnn_malaria.h5")
 
-image_path = '../AI PBL project/cell_images/Uninfected/C1_thinF_IMG_20150604_104722_cell_73.png'
+image_path = '../AI-diseases-prediction-master/cell_images/Uninfected/C1_thinF_IMG_20150604_104722_cell_73.png'
 #image_path = '../AI PBL project/cell_images/Parasitized/C33P1thinF_IMG_20150619_115808a_cell_205.png'
 Xtest = convert_img(image_path)
 image_result = reconstructed_model.predict(Xtest)
