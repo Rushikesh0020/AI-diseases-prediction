44dd3564d81071a4131a54cd261b04524c69be98
desaidj <desaidj96@outlook.com>
2020-11-25 12:59:51 +0530

    adding basic template and new softmax xray model

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from tempfile import TemporaryDirectory
+from flask.globals import request
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import tensorflow as tf
@@ -11,6 +13,11 @@ import h5py
 import matplotlib.pyplot as plt
 #import seaborn as sns
 from sklearn.model_selection import train_test_split
+from flask import Flask
+from flask import request, redirect,render_template
+from werkzeug.utils import secure_filename
+import tempfile
+tempdirectory = tempfile.gettempdir()
 
 def convert_img(image_path, target_size=(128,128)):
     img_arr_img = []
@@ -20,13 +27,46 @@ def convert_img(image_path, target_size=(128,128)):
     img_arr_img.append(image)
     return np.asarray(img_arr_img)
 
-reconstructed_model = keras.models.load_model("../AI-diseases-prediction-master/pretrained models/cnn_malaria.h5")
 
-image_path = '../AI-diseases-prediction-master/cell_images/Uninfected/C1_thinF_IMG_20150604_104722_cell_73.png'
-#image_path = '../AI-diseases-prediction-master/cell_images/Parasitized/C33P1thinF_IMG_20150619_115808a_cell_205.png'
-Xtest = convert_img(image_path)
-image_result = reconstructed_model.predict(Xtest)
-if(image_result > 0.5):
-    print("Infected")
-else:
-    print("Not infected")
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    errors = []
+    results = ''
+    methodSelected = request.form.get('methodSelected')
+    image_path = ''
+    if request.method == "POST":
+        file = request.files['file']
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(tempdirectory, filename))
+        image_path = os.path.join(tempdirectory, filename)
+            # os.remove(filename)
+            # file=open(os.path.join(tempdirectory, filename), encoding="utf-8")
+            # textImport = file.read()
+        
+        
+        if methodSelected == 'malaria':
+            reconstructed_model = keras.models.load_model("../AI-diseases-prediction-master/pretrained models/cnn_malaria.h5")
+            #image_path = '../AI-diseases-prediction-master/cell_images/Uninfected/C1_thinF_IMG_20150604_104722_cell_73.png'
+            #image_path = '../AI-diseases-prediction-master/cell_images/Parasitized/C33P1thinF_IMG_20150619_115808a_cell_205.png'
+            Xtest = convert_img(image_path)
+            image_result = reconstructed_model.predict(Xtest)
+            if(image_result > 0.5):
+                results = 'Infected'
+            else:
+                results = "Not infected"
+                
+
+        if methodSelected == 'xray':
+            print("Still under development")
+                #############################################################################
+            ###
+            
+            ###
+      
+
+    return render_template('index.html', results = results)
+
+
